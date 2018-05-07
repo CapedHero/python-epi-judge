43d00fae4ce51291e0395efae2bd723f99251597
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2018-05-07 11:53:43 +0200

    4.2 Swap bits - MW solution

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -1,12 +1,17 @@
-from test_framework import generic_test
-
-
-def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+def swap_bits(integer: int, bit_index_1: int, bit_index_2: int):
+    """Least significant bit is at index `0`."""
+    # If bits at indexes are the same, there is no need to swap.
+    if (integer >> bit_index_1) & 1 == (integer >> bit_index_2) & 1:
+        return integer
+    swap_bit_mask: int = 1 << bit_index_1 | 1 << bit_index_2
+    swapped_bits_integer: int = integer ^ swap_bit_mask
+    return swapped_bits_integer
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("swap_bits.py", 'swap_bits.tsv',
-                                       swap_bits))
+    from sys import exit
+
+    from test_framework import generic_test
+
+    exit(generic_test.generic_test_main(
+        "swap_bits.py", 'swap_bits.tsv', swap_bits))
