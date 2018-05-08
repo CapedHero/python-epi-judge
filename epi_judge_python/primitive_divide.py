7e8b2bb13e5a5d55ed2ea17510b16c8f422655a2
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2018-05-08 22:49:12 +0200

    Compute x/y - MW solution

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -1,12 +1,36 @@
-from test_framework import generic_test
+from typing import Tuple
 
 
-def divide(x, y):
-    # TODO - you fill in here.
-    return 0
+def divide(dividend: int, divisor: int) -> int:
+    quotient: int = 0
+    remaining_dividend: int = dividend
+    quotient_to_add: int
+    while remaining_dividend >= divisor:
+        remaining_dividend, quotient_to_add = fit_squares_of_divisor(
+            remaining_dividend,
+            divisor,
+        )
+        quotient += quotient_to_add
+
+    return quotient
+
+
+def fit_squares_of_divisor(dividend: int, divisor: int) -> Tuple[int, int]:
+    quotient: int = 1
+    divisor_squares: int = divisor
+    divisor_square_lookahead: int = divisor_squares << 1
+
+    while dividend >= divisor_square_lookahead:
+        quotient <<= 1
+        divisor_squares = divisor_square_lookahead
+        divisor_square_lookahead <<= 1
+
+    remaining_dividend: int = dividend - divisor_squares
+    return remaining_dividend, quotient
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("primitive_divide.py",
-                                       "primitive_divide.tsv", divide))
+    from test_framework import generic_test
+
+    exit(generic_test.generic_test_main(
+        "primitive_divide.py", "primitive_divide.tsv", divide))
