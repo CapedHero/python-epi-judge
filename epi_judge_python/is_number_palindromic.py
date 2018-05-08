ccff1ac50d630f587c8350f3f106cfb0e697b4ec
Maciej Wrześniewski <mc.wrzesniewski@gmail.com>
2018-05-08 09:42:42 +0200

    4.9 Check if a decimal integer is a palindrome

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,13 +1,16 @@
-from test_framework import generic_test
-
-
-def is_palindrome_number(x):
-    # TODO - you fill in here.
-    return True
+def is_palindrome_number(integer: int) -> bool:
+    if integer < 0:
+        return False
+    integer_str: str = str(integer)
+    reversed_integer_str: str = ''.join(reversed(integer_str))
+    return integer_str == reversed_integer_str
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_number_palindromic.py",
-                                       "is_number_palindromic.tsv",
-                                       is_palindrome_number))
+    from test_framework import generic_test
+
+    exit(generic_test.generic_test_main(
+        "is_number_palindromic.py",
+        "is_number_palindromic.tsv",
+        is_palindrome_number
+    ))
